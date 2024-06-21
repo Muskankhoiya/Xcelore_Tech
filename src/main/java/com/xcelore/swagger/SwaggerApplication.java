5074d9a3fc717f01edc18304bfcacea91c06859c
Muskankhoiya <muskankhoiya@gmail.com>
2024-06-21 18:17:12 +0530

    first commit

---
+++ src/main/java/com/xcelore/swagger/SwaggerApplication.java
@@ -0,0 +1,18 @@
+package com.xcelore.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApplication.class, args);
+	}
+
+}
